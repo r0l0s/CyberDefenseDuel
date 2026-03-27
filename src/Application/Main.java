50ed01ec17c0ce4d31e508e7a995c4dc5a229f98
Ezequiel <ezequielbv2305@gmail.com>
2026-03-26 22:24:00 -0600

    feat: Enemigos ya poseen distintos tipos y pueden disparar al enemigo.

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -27,6 +27,7 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Translate;
 import javafx.util.Duration;
+import java.math.*;
 
 
 public class Main extends Application {
@@ -42,7 +43,7 @@ public class Main extends Application {
 			Player player = Player.get_instance();
 			player.get_colider().setFocusTraversable(true);
 			
-			Bullet balaMala = new Bullet(100.0f,-300.0f,1);
+			Bullet balaMala = new Bullet(100.0f,70.0f,1);
 			balaMala.get_colider().setFill(Color.GREEN);
 			balaMala.setType(2);
 
@@ -50,20 +51,19 @@ public class Main extends Application {
 			root.getChildren().add(player.get_colider());
 			
 			//Enemy test
-			enemyList = new LinkedList<Enemy>(new Enemy(200, 40));
+			enemyList = new LinkedList<Enemy>(new Enemy(200, 40,0));
 			root.getChildren().add(enemyList.get(0).get_colider());
-			enemyList.get(0).get_colider().setFill(Color.GREEN);
-			for(int i=1;i<2;i++){
+			for(int i=1;i<9;i++){
 				int lastx = (int) enemyList.get(i-1).get_colider().getX();
-				Enemy enemy = new Enemy(lastx+100, 40);
+				int ranint = (int)(Math.random() * ((2 - 0) + 1)) + 0 ;
+				Enemy enemy = new Enemy(lastx+100, 40,ranint);
 				enemyList.insert(enemy);
-				enemy.get_colider().setFill(Color.GREEN);
 				root.getChildren().add(enemy.get_colider());
 			}
 			//Enemy bullets
-			free_enemyBullets = new StackList<Bullet>(new Bullet(100.0f,-300.0f,1));
+			free_enemyBullets = new StackList<Bullet>(new Bullet(100.0f,150,1));
 			for(int i=1;i<19;i++){
-				free_enemyBullets.push(new Bullet(100.0f,-300.0f,1));
+				free_enemyBullets.push(new Bullet(100.0f,150,1));
 			}
 			
 
@@ -87,12 +87,16 @@ public class Main extends Application {
 				//Fue la prueba para colisiones lo voy a dejar así por ahora
 				Bounds areaBusqueda = new BoundingBox(100, 0, 5, 5);
 
-				//Prueba2 de bala jugador segunda manera
-				int player_bulletIndex = 0;
+				int enemy_count = 0;
 				
 				@Override
 			    public void handle(long now) {
-					//Time configuration
+					// 1. Capturar Input (Teclas, ratón)
+			        // 2. Actualizar lógica (Movimiento, Colisiones)
+			        // 3. Renderizar (JavaFX lo hace automático al mover nodos)
+					//Input-Update-Colitions-Cleanup
+
+					//region Time configuration
 					if (lastTime == 0) {
 						lastTime = now;
 						return;
@@ -101,17 +105,26 @@ public class Main extends Application {
 					lastTime = now;
 					timerAcumulado += deltaTime;
 					tiempo2+=deltaTime;
-
-			        // 1. Capturar Input (Teclas, ratón)
-			        // 2. Actualizar lógica (Movimiento, Colisiones)
-			        // 3. Renderizar (JavaFX lo hace automático al mover nodos)
-					//Input-Update-Colitions-Cleanup
-					
+					//endregion
+			        
+					//Inputs:
 			    	player.move(inputs.dir);
-					//balaMala.move();
 					
-					//System.out.println(get_object(100, 150));
-					
+					if(inputs.get_shoot()) {
+			    		System.out.println("Jugador disparó");
+						player.set_type(inputs.get_type());
+						player.shoot();
+			    	}
+
+					//Timers y calculos de tiempo
+					if(timerAcumulado<5.0){
+						if (inputs.get_shoot()){
+							inputs.set_shoot(false);
+						}
+						timerAcumulado = 0;
+					}
+
+					//region Movimiento Enemigo
 					//Enemy test: Izq: -300 Der:50
 					for(int i=0;i<enemyList.getSize();i++){
 						Enemy enemy = enemyList.get(i);
@@ -121,25 +134,10 @@ public class Main extends Application {
 							enemy.changeDir();
 						}
 					}
-
-			    	if(inputs.get_shoot()) {
-			    		System.out.println("Jugador disparó");
-						player.set_type(inputs.get_type());
-						player.shoot();
-			    	}
+					//endregion
+			    	
 					
-					//region 
-					if (root.getChildren().contains(balaMala.get_colider())){
-
-						if (balaMala.get_colider().getBoundsInParent().intersects(player.get_colider().getBoundsInParent())) {
-							root.getChildren().remove(balaMala.get_colider());
-							enable_move = false;
-							player.damage(10);
-							//balaMala.get_colider().setLayoutY(-300.0f);
-							
-						}
-					}
-
+					//region Lógica Balas del jugador
 					//Verificar las balas que están en juego del jugador
 					if (player.get_usedBullets() != null){
 						for(int i=0;i<player.get_usedBullets().getSize();i++){
@@ -180,7 +178,9 @@ public class Main extends Application {
         					}
 						}
 					}
+					//endregion 
 
+					//region Lógica Balas del enemigo
 					//Verificar las balas enemigas que están en juego
 					if (used_enemyBullets != null){
 						//Comprobar como están y moverlas
@@ -208,13 +208,14 @@ public class Main extends Application {
 					}
 
 					//Cambiar nombre de variable
-					if(tiempo2>3.0){
+					if(tiempo2>1.5){
 						//Comprobaciones de tiempo
 						//System.out.println("Pasaron 3 segundos"+balaMala.getType());
 						tiempo2 =0;
 						//Sacar del stack a la lista
 						if(free_enemyBullets.getSize()!=0){
 							Bullet bullet = free_enemyBullets.top();
+							Rectangle enemy = enemyList.get(enemy_count).get_colider();
 							//Rectangle colider =  get_instance().get_colider();
 							free_enemyBullets.pop();
 							if (used_enemyBullets == null){
@@ -222,21 +223,19 @@ public class Main extends Application {
 							}else{
 								used_enemyBullets.insert(bullet);
 							}
-							//bullet.setPosition(colider.getLayoutX() + colider.getTranslateX(), this.get_colider().getLayoutY());
-							bullet.setPosition(100.0f,-300.0f);
-							bullet.setType(2);
 							
+							bullet.setPosition(enemy.getX()+enemy.getTranslateX()-80,enemy.getY()-70);
+							bullet.setType(enemyList.get(enemy_count).getAttackType());
+							enemy_count++;
+							if (enemy_count>=enemyList.getSize()){
+								enemy_count=0;
+							}
 						}
 					}
 					//endregion
-
-					if(timerAcumulado<5.0){
-						if (inputs.get_shoot()){
-							inputs.set_shoot(false);
-						}
-						timerAcumulado = 0;
-					}
+							
 			}
+			
 			private Node get_object(double x, double y,Node self){
 				Node obj = root.lookupAll("*").stream()
 					.filter(node -> node != root)
--- src/Game/Enemy.java
+++ src/Game/Enemy.java
@@ -1,19 +1,41 @@
 package Game;
 
+import javafx.scene.paint.Color;
+
 public class Enemy extends GameObject{
 
-    private int vel;
+    private double vel;
     private int dir;
+    private int attack_type;
 
-    public Enemy(float x, float y) {
+    public Enemy(float x, float y, int type) {
         super(75.0f,75.0f);
         this.colider.setX(x);
 		this.colider.setY(y);
 		this.vel =  2;
         this.dir = -1;
+        this.attack_type = type;    
+        //Preventivo
+        switch (type) {
+            case 0:
+                this.colider.setFill(Color.RED);
+                break;
+            case 1:
+                this.colider.setFill(Color.BLUE);
+                break;
+            case 2:
+                this.colider.setFill(Color.YELLOW);
+                break;
+            default:
+                this.colider.setFill(Color.GREEN);
+                break;
+        }
     }
 
     //Getters and Setters section:
+    public int getAttackType(){
+        return this.attack_type;
+    }
 
     //Change the position of the bullet on the X axis.
 	public void move() {
